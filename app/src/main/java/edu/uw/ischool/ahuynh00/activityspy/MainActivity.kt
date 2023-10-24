f04a8e29f03fff39792a473ae791c0071d111834
AHUYNH00 <AHUYNH00@UW.EDU>
2023-10-23 18:00:29 -0700

    screenshots and function overrides

---
+++ app/src/main/java/edu/uw/ischool/ahuynh00/activityspy/MainActivity.kt
@@ -0,0 +1,43 @@
+package edu.uw.ischool.ahuynh00.activityspy
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.i(TAG, "onCreate event fired")
+        Log.i(TAG, "onCreate param: $savedInstanceState")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "onDestroy event fired")
+        Log.e(TAG, "We're going down, Captain!")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart event fired")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop event fired")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause event fired")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume event fired")
+    }
+
+}
